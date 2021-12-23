a7a4773db4bcbf7717d401d3bedce42ded89e846
史瑞琪 <una@lambda.local>
2021-12-23 18:08:15 +0800

    add all

---
+++ Tankweb/Assets/Scripts/mainmune.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainmune : MonoBehaviour
+{
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+    public void PlayFlagame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Play3game()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void QuitGame() {
+        Debug.Log("Quit");
+        Application.Quit();
+
+    }
+    
+}
---
+++ Tankweb/Assets/Scripts/quitcontrol.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class quitcontrol : MonoBehaviour
+{
+    public void doublequit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void threequit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+
+    public void flagquit()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+    // Start is called before the first frame update
+}
